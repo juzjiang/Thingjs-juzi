9a72db0b62493d9d7f77313d92fc416d059a1a5e
changjuhong <changjuhong@uino.com>
2024-06-20 15:41:22 +0800

    1212

--- uploads/wechat/5qmY5a2Q6YWx/file/测试离线开发包/测试离线开发包.cps
+++ uploads/wechat/5qmY5a2Q6YWx/file/测试离线开发包/测试离线开发包.cps
@@ -9,13 +9,12 @@
     // 举例：项目中引用的第一个场景URL为 "/api/scene/d370cad09e74f42d932b083d"，
     //      第二个场景URL为 "/api/scene/b422fd26d4c7874df3992068"，
     //      为能正确打包上述两个场景，需配置如下：
-    // "scenes": [
-    //     "/api/scene/d370cad09e74f42d932b083d",
-    //     "/api/scene/b422fd26d4c7874df3992068"
-    //  ],
+     "scenes": [
+         "/api/scene/production_211848",
+     ],
     // 举例：项目中动态引用了模型 "/api/models/8CF6171F7EE046968B16E10181E8D941/0/gltf/"，
     //      为能正确打包该引用模型，需配置如下：
-    // "models": [
-    //     "/api/models/8CF6171F7EE046968B16E10181E8D941/0/gltf/"
-    //  ]
+     "models": [
+         "/Resources/Model/admin/39e7a01c022747518a479548380c32f0"
+      ]
 }
--- 测试离线开发包.js
+++ 测试离线开发包.js
@@ -95,17 +95,35 @@
 // });
 
 // 创建Thing
+// var app = new THING.App();
+// var obj = app.create({
+//     type: 'Thing',
+//     name: '01_1_-1qian1', 
+//     url:'/Resources/Model/admin/5d290f27c3404aa89f8c09388b59981e',
+//     position: [0, 0, 0],
+//     angle: 0,
+//     complete: function () {
+//       // console.log('thing created: ' + this.id);
+//       // 单资源加载模型，摄像机看向目标物体
+//       app.camera.fit(obj);
+//       obj.addControl(new THING.AxisTransformControl(obj), 'axisControl');
+//     }
+// });
+// 离线开发引入园区场景
+var app = new THING.App({
+  url: '/api/scene/production_211848'  // 场景地址
+});
+// 创建Thing
 var app = new THING.App();
 var obj = app.create({
     type: 'Thing',
-    name: '01_1_-1qian1', 
-    url:'/Resources/Model/admin/5d290f27c3404aa89f8c09388b59981e',
+    name: '01hou01', 
+    url:'/Resources/Model/admin/39e7a01c022747518a479548380c32f0',
     position: [0, 0, 0],
     angle: 0,
     complete: function () {
       // console.log('thing created: ' + this.id);
       // 单资源加载模型，摄像机看向目标物体
-      app.camera.fit(obj);
-      obj.addControl(new THING.AxisTransformControl(obj), 'axisControl');
+      // app.camera.fit(obj);
     }
 });
